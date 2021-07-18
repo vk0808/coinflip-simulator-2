142e967cccaf76dc68fbd20cf3f6f9d5d3286926
Vinod Kumar R <vk4765712@gmail.com>
2021-07-18 16:56:58 +0530

    added: flip generate

--- coinFlipSimulator.sh
+++ coinFlipSimulator.sh
@@ -1,3 +1,17 @@
 #! /bin/bash -x
 
 echo "Welcome to Coin Flip Simulator Program"
+
+# Variable
+HEAD=1
+
+# Generate Head/Tail
+coinFlip=$(( $RANDOM %2 ))
+
+
+#Check if it's Head or Tail
+if [ $coinFlip -eq $HEAD ]; then
+ 	echo "heads"
+else
+	echo "tails"
+fi
